10a5a0adaa5d6b250b20b77e4f295ec049f6e2d6
Bruno Afonso <brunohafonso@gmail.com>
2020-09-24 11:11:43 -0300

    chore: add the jest base config to run tests on project

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+const { resolve } = require('path');
+
+const rootDir = resolve(__dirname);
+
+module.exports = {
+  rootDir,
+  displayName: 'root-tests',
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageDirectory: 'coverage/unit',
+  coveragePathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+    'src/index.ts',
+    '__tests__',
+    'src/@types',
+    'src/interfaces'
+  ],
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        pageTitle: 'Desafio técnico API',
+        publicPath: './html-report',
+        filename: 'unit.html',
+        expand: true,
+      },
+    ],
+  ],
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@tests/(.*)': '<rootDir>/__tests__/$1',
+  },
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/*.spec.ts'],
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+};
