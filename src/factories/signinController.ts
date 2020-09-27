0167b88eb88d7624e036c100d9e083af53fb060d
Bruno Afonso <brunohafonso@gmail.com>
2020-09-27 20:10:33 -0300

    refactor: add the dependency injection that was missing (joiAdapter)

--- src/factories/signinController.ts
+++ src/factories/signinController.ts
@@ -3,15 +3,18 @@ import AuthProvider from '@src/providers/AuthProvider';
 import EncryptProvider from '@src/providers/EncryptProvider';
 import UserRepository from '@src/repositories/UserRepository';
 import AuthenticateUserService from '@src/services/AuthenticateUserService';
+import JoiAdapter from '@src/utils/JoiAdapter';
 
 export default (): SigninController => {
   const userRepository = new UserRepository();
   const authProvider = new AuthProvider();
   const encryptProvider = new EncryptProvider();
+  const joiAdapter = new JoiAdapter();
   const authenticateUserService = new AuthenticateUserService(
     userRepository,
     authProvider,
     encryptProvider,
+    joiAdapter,
   );
   return new SigninController(authenticateUserService);
 };
