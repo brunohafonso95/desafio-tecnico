255becf9f167ba3e4538f8eb74f3c2d3a085d4d6
Bruno Afonso <brunohafonso@gmail.com>
2020-09-28 00:26:16 -0300

    refactor: add logger to middleware of not found route

--- src/controllers/NotFoundController.ts
+++ src/controllers/NotFoundController.ts
@@ -2,11 +2,13 @@ import httpStatus from 'http-status-codes';
 
 import BaseController from '@src/abstracts/BaseController';
 import { IHttpRequest, IHttpResponse, IController } from '@src/interfaces';
+import Logger from '@src/utils/Logger';
 
 export default class NotFoundController
   extends BaseController
   implements IController {
-  public handleRoute(_httpRequest: IHttpRequest): Promise<IHttpResponse> {
+  public async handleRoute(_httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    Logger.warn({ msg: 'rota não encontrada' });
     return Promise.resolve(
       this.formatGenericErrorResponse('Not Found', httpStatus.NOT_FOUND),
     );
