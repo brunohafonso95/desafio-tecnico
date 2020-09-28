543287cdbeee0c28160835b6ba37b56e4038841b
Bruno Afonso <brunohafonso@gmail.com>
2020-09-27 23:14:20 -0300

    feat: add the logs of steps on appliction startup

--- src/App.ts
+++ src/App.ts
@@ -25,11 +25,13 @@ export default class App {
   private setupGlobalMiddlewares(): void {
     Logger.info({ msg: 'Setuping global middlewares' });
     this.server.use(bodyParserMiddleware());
+    Logger.info({ msg: 'Global middlewares setup was successfully finished' });
   }
 
   private docsSetup(): void {
     Logger.info({ msg: 'Setuping the application docs' });
     this.server.use('/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
+    Logger.info({ msg: 'Application docs setup was successfully finished' });
   }
 
   private setupRoutes(): void {
@@ -37,22 +39,30 @@ export default class App {
     this.server.use('/api/v1', signupRoutes);
     this.server.use('/api/v1', signinRoutes);
     this.server.use('/api/v1', getUserRoutes);
+    Logger.info({ msg: 'Application routes setup was successfully finished' });
   }
 
   private setupGlobalErrorMiddleware(): void {
     Logger.info({ msg: 'Setuping global error middleware' });
     this.server.use(errorMiddleware);
+    Logger.info({
+      msg: 'Global Error middleware setup was successfully finished',
+    });
   }
 
   private setupNotFoundMiddleware(): void {
     Logger.info({ msg: 'Setuping not found error middleware' });
     const notFoundController = new NotFoundController();
     this.server.use(expressRouteAdapter(notFoundController));
+    Logger.info({
+      msg: 'Not found error middleware setup was successfully finished',
+    });
   }
 
   private validateEnvVariables(): void {
     Logger.info({ msg: 'Validating env variables' });
     validateEnvVariables();
+    Logger.info({ msg: 'The env variables was validated with success' });
   }
 
   public async initApplication(): Promise<void> {
@@ -64,16 +74,21 @@ export default class App {
     this.setupNotFoundMiddleware();
     this.setupGlobalErrorMiddleware();
     await this.databaseSetup();
+    Logger.info({
+      msg: 'The inital setup of application was finished successfully',
+    });
   }
 
   private async databaseSetup(): Promise<void> {
     Logger.info({ msg: 'Connecting to database' });
     await mongo.connect();
+    Logger.info({ msg: 'Database connected successfully' });
   }
 
   public async closeApplication(): Promise<void> {
     Logger.info({ msg: 'Closing the application' });
     await mongo.close();
+    Logger.info({ msg: 'Database disconnected successfully' });
   }
 
   public startServer(): void {
